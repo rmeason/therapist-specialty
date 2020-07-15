af9934b2e900be20f303ffbdcf2001d98a44763b
rmeason <rme15a@acu.edu>
2020-07-15 10:06:05 -0500

    profile cards

---
+++ client/src/components/SearchBtn.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./SearchBtn.css";
+
+function SearchBtn(props) {
+
+    return (
+
+        <div class="input-group input-group-rounded">
+        <input class="input-group-field" type="search" />
+        <div class="input-group-button">
+            <input type="submit" class="button secondary" value="Search" />
+        </div>
+        </div>
+
+    );
+}
+
+export default SearchBtn;
--- client/src/components/pages/Search.js
+++ client/src/components/pages/Search.js
@@ -1,8 +1,49 @@
 import React from 'react';
+import SearchBtn from "../SearchBtn"
+import ProfileCard from "../ProfileCard"
+
+import "./Search.css"
 
 const Search = () => (
 
-<h2>Search Page</h2>
+    <div>
+    
+        <SearchBtn />
+
+        <section class="four-up-feature">
+            <div class="row four-up-feature-header align-center">
+                <div class="medium-8 columns">
+                <h2>Your Local Providers</h2>
+                <h5> Below is a list of providers in your area who specialize in the symptoms you display:</h5>
+                </div>
+            </div>
+            <div class="row align-spaced">
+                <div class="small-10 medium-3 columns">
+                
+                    <ProfileCard />
+                
+                </div>
+                <div class="small-10 medium-3 columns">
+                
+                    <ProfileCard />
+                
+                </div>
+                <div class="small-10 medium-3 columns">
+                
+                    <ProfileCard />
+                
+                </div>
+                <div class="small-10 medium-3 columns">
+
+                    <ProfileCard />
+
+                </div>
+            </div>
+        </section>
+
+
+
+    </div>
 
 );
 
