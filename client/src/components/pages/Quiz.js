42dc493a0823dbd4f07c7799925e3e84c818aea7
rmeason <rme15a@acu.edu>
2020-07-16 00:14:54 -0500

    added content to quiz page and stylized the page

--- client/src/components/pages/Quiz.js
+++ client/src/components/pages/Quiz.js
@@ -1,67 +1,165 @@
-import React from 'react';
+import React from "react";
+import "./Quiz.css"
+
+
+const Quiz = () => (
+
+<div class="wrap">
+  <h1 class="likert-header">Please answer a few questions:</h1>
+  <form action="">
+    <label class="statement">Are you more anxious or depressed?</label>
+    <ul class='likert'>
+      <li>
+        <input type="radio" name="likert" value="strong_agree" />
+        <label>Anxious</label>
+      </li>
+      <li>
+        <input type="radio" name="likert" value="strong_agree" />
+        <label></label>
+      </li>
+      <li>
+        <input type="radio" name="likert" value="strong_agree" />
+        <label>Neutral</label>
+      </li>
+      <li>
+        <input type="radio" name="likert" value="disagree" />
+        <label></label>
+      </li>
+      <li>
+        <input type="radio" name="likert" value="strong_agree" />
+        <label>Depressed</label>
+      </li>
+    </ul>
+    <label class="statement">Are you more happy or sad?</label>
+    <ul class='likert'>
+      <li>
+        <input type="radio" name="likert" value="strong_agree" />
+        <label>Happy</label>
+      </li>
+      <li>
+        <input type="radio" name="likert" value="strong_agree" />
+        <label></label>
+      </li>
+      <li>
+        <input type="radio" name="likert" value="strong_agree" />
+        <label>Neutral</label>
+      </li>
+      <li>
+        <input type="radio" name="likert" value="disagree" />
+        <label></label>
+      </li>
+      <li>
+        <input type="radio" name="likert" value="strong_agree" />
+        <label>Sad</label>
+      </li>
+    </ul>
+    <label class="statement">Are you more worried or calm?</label>
+    <ul class='likert'>
+      <li>
+        <input type="radio" name="likert" value="strong_agree" />
+        <label>Worried</label>
+      </li>
+      <li>
+        <input type="radio" name="likert" value="strong_agree" />
+        <label></label>
+      </li>
+      <li>
+        <input type="radio" name="likert" value="strong_agree" />
+        <label>Neutral</label>
+      </li>
+      <li>
+        <input type="radio" name="likert" value="disagree" />
+        <label></label>
+      </li>
+      <li>
+        <input type="radio" name="likert" value="strong_agree" />
+        <label>Calm</label>
+      </li>
+    </ul>
+    <div class="buttons">
+      <button class="clear">Clear</button>
+      <button class="submit">Submit</button>
+    </div>
+  </form>
+</div>
+
+);
+
+export default Quiz;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
 // import PropTypes from 'prop-types';
 // import Question from "../Question";
 // import QuestionCount from '../QuestionCount';
 // import AnswerOption from '../AnswerOption';
 
 
-    // function Quiz(props) {
-    //     function renderAnswerOptions(key) {
-    //         return (
-    //           <AnswerOption
-    //             key={key.content}
-    //             answerContent={key.content}
-    //             answerType={key.type}
-    //             answer={props.answer}
-    //             questionId={props.questionId}
-    //             onAnswerSelected={props.onAnswerSelected}
-    //           />
-    //         );
-    //       }
-    //       return (
-    //         <div className="quiz">
-    //           <QuestionCount
-    //             counter={props.questionId}
-    //             total={props.questionTotal}
-    //           />
-    //           <Question content={props.question} />
-    //           <ul className="answerOptions">
-    //             {props.answerOptions.map(renderAnswerOptions)}
-    //           </ul>
-    //         </div>
-    //     );
-    //   }
-      
-    //   Quiz.propTypes = {
-    //     answer: PropTypes.string.isRequired,
-    //     answerOptions: PropTypes.array.isRequired,
-    //     counter: PropTypes.number.isRequired,
-    //     question: PropTypes.string.isRequired,
-    //     questionId: PropTypes.number.isRequired,
-    //     questionTotal: PropTypes.number.isRequired,
-    //     onAnswerSelected: PropTypes.func.isRequired
-    //   };
 
-    const Quiz = () => (
 
-        <div class="about-the-author">
-        <h3 class="author-title">Quiz Page</h3>
-        <div class="row">
-            <div class="small-12 medium-4 columns">
-            <div class="author-image">
-                {/* <img src={depression} /> */}
-            </div>
-            </div>
-            <div class="small-12 medium-8 columns">
-            <h4 class="separator-left">This is the quiz page</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores eum, iusto vel repudiandae, quaerat soluta sequi officia. Blanditiis atque, illo eaque sint in architecto illum nostrum repudiandae labore tenetur! Eaque impedit pariatur odio ad ipsum qui aspernatur dolorem consequuntur a molestias nisi, quae voluptatem expedita, inventore voluptatibus dolores, veritatis corporis facilis laudantium explicabo vero! Non hic quia facilis blanditiis eum.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores eum, iusto vel repudiandae, quaerat soluta sequi officia. Blanditiis atque, illo eaque sint in architecto illum nostrum repudiandae labore tenetur! Eaque impedit pariatur odio ad ipsum qui aspernatur dolorem consequuntur a molestias nisi, quae voluptatem expedita, inventore voluptatibus dolores, veritatis corporis facilis laudantium explicabo vero! Non hic quia facilis blanditiis eum.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores eum, iusto vel repudiandae, quaerat soluta sequi officia. Blanditiis atque, illo eaque sint in architecto illum nostrum repudiandae labore tenetur! Eaque impedit pariatur odio ad ipsum qui aspernatur dolorem consequuntur a molestias nisi, quae voluptatem expedita, inventore voluptatibus dolores, veritatis corporis facilis laudantium explicabo vero! Non hic quia facilis blanditiis eum.</p>
-            </div>
-        </div>
-        </div>
+// render (
+//     <div className="App">
+//       <div className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h2>React Quiz</h2>
+//       </div>
+//       <Question content="What is your favourite food?" />
+//     </div>
+//   );
+
+
+
+
+
+//     const Quiz = () => (
+
+//         <div class="about-the-author">
+//         <h3 class="author-title">Quiz Page</h3>
+//         <div class="row">
+//             <div class="small-12 medium-4 columns">
+//             <div class="author-image">
+//                 {/* <img src={depression} /> */}
+//             </div>
+//             </div>
+//             <div class="small-12 medium-8 columns">
+//             <h4 class="separator-left">This is the quiz page</h4>
+//             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores eum, iusto vel repudiandae, quaerat soluta sequi officia. Blanditiis atque, illo eaque sint in architecto illum nostrum repudiandae labore tenetur! Eaque impedit pariatur odio ad ipsum qui aspernatur dolorem consequuntur a molestias nisi, quae voluptatem expedita, inventore voluptatibus dolores, veritatis corporis facilis laudantium explicabo vero! Non hic quia facilis blanditiis eum.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores eum, iusto vel repudiandae, quaerat soluta sequi officia. Blanditiis atque, illo eaque sint in architecto illum nostrum repudiandae labore tenetur! Eaque impedit pariatur odio ad ipsum qui aspernatur dolorem consequuntur a molestias nisi, quae voluptatem expedita, inventore voluptatibus dolores, veritatis corporis facilis laudantium explicabo vero! Non hic quia facilis blanditiis eum.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores eum, iusto vel repudiandae, quaerat soluta sequi officia. Blanditiis atque, illo eaque sint in architecto illum nostrum repudiandae labore tenetur! Eaque impedit pariatur odio ad ipsum qui aspernatur dolorem consequuntur a molestias nisi, quae voluptatem expedita, inventore voluptatibus dolores, veritatis corporis facilis laudantium explicabo vero! Non hic quia facilis blanditiis eum.</p>
+//             </div>
+//         </div>
+//         </div>
     
     
     
-    );
+//     );
 
 
-export default Quiz;
+// export default Quiz;
