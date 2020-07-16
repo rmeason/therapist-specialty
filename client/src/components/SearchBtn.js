08ed8158f502e6c6b4a837fc71b5aa570cc5608d
rmeason <rme15a@acu.edu>
2020-07-16 10:19:20 -0500

    quiz now allows for multiple answers

--- client/src/components/SearchBtn.js
+++ client/src/components/SearchBtn.js
@@ -6,7 +6,7 @@ function SearchBtn(props) {
     return (
 
         <div class="input-group input-group-rounded">
-        <input class="input-group-field" type="search" />
+            <input class="input-group-field" type="search"  {...props} />
         <div class="input-group-button">
             <input type="submit" class="button secondary" value="Search" />
         </div>
--- client/src/components/pages/Quiz.js
+++ client/src/components/pages/Quiz.js
@@ -10,69 +10,69 @@ const Quiz = () => (
     <label class="statement">Are you more anxious or depressed?</label>
     <ul class='likert'>
       <li>
-        <input type="radio" name="likert" value="1" />
+        <input type="radio" name="likert1" value="1" />
         <label>Anxious</label>
       </li>
       <li>
-        <input type="radio" name="likert" value="2" />
+        <input type="radio" name="likert1" value="2" />
         <label></label>
       </li>
       <li>
-        <input type="radio" name="likert" value="3" />
+        <input type="radio" name="likert1" value="3" />
         <label>Neutral</label>
       </li>
       <li>
-        <input type="radio" name="likert" value="4" />
+        <input type="radio" name="likert1" value="4" />
         <label></label>
       </li>
       <li>
-        <input type="radio" name="likert" value="5" />
+        <input type="radio" name="likert1" value="5" />
         <label>Depressed</label>
       </li>
     </ul>
     <label class="statement">Are you more happy or sad?</label>
     <ul class='likert'>
       <li>
-        <input type="radio" name="likert" value="1" />
+        <input type="radio" name="likert2" value="1" />
         <label>Happy</label>
       </li>
       <li>
-        <input type="radio" name="likert" value="2" />
+        <input type="radio" name="likert2" value="2" />
         <label></label>
       </li>
       <li>
-        <input type="radio" name="likert" value="3" />
+        <input type="radio" name="likert2" value="3" />
         <label>Neutral</label>
       </li>
       <li>
-        <input type="radio" name="likert" value="4" />
+        <input type="radio" name="likert2" value="4" />
         <label></label>
       </li>
       <li>
-        <input type="radio" name="likert" value="5" />
+        <input type="radio" name="likert2" value="5" />
         <label>Sad</label>
       </li>
     </ul>
     <label class="statement">Are you more worried or calm?</label>
     <ul class='likert'>
       <li>
-        <input type="radio" name="likert" value="1" />
+        <input type="radio" name="likert3" value="1" />
         <label>Worried</label>
       </li>
       <li>
-        <input type="radio" name="likert" value="2" />
+        <input type="radio" name="likert3" value="2" />
         <label></label>
       </li>
       <li>
-        <input type="radio" name="likert" value="3" />
+        <input type="radio" name="likert3" value="3" />
         <label>Neutral</label>
       </li>
       <li>
-        <input type="radio" name="likert" value="4" />
+        <input type="radio" name="likert3" value="4" />
         <label></label>
       </li>
       <li>
-        <input type="radio" name="likert" value="5" />
+        <input type="radio" name="likert3" value="5" />
         <label>Calm</label>
       </li>
     </ul>
